caf07f40e13fcd230c231c05a20719987e78e0cb
Senhui <huangsenhui@live.com>
2023-06-04 09:52:32 +0800

    Initial Commit

---
+++ MyProject/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MyProject
+//
+//  Created by HuangSenhui on 2023/6/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyProject/MyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyProjectApp.swift
+//  MyProject
+//
+//  Created by HuangSenhui on 2023/6/4.
+//
+
+import SwiftUI
+
+@main
+struct MyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
